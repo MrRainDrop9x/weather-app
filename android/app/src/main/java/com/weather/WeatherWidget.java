81269fafc19b8387ee61666dc78f373897113d34
MrSunfat <trannhatdanh15062001@gmail.com>
2022-05-16 12:40:13 +0700

    Widget basic

---
+++ android/app/src/main/java/com/weather/WeatherWidget.java
@@ -0,0 +1,49 @@
+package com.weather;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WeatherWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        try {
+            SharedPreferences sharedPref = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
+            String appString = sharedPref.getString("appData", "{\"text\":'no data', \"temp\":'no data'}");
+            JSONObject appData = new JSONObject(appString);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
+            views.setTextViewText(R.id.appwidget_text_city, appData.getString("text"));
+            views.setTextViewText(R.id.appwidget_text_temp, appData.getString("temp"));
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
